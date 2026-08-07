e8947e8160a2075059988f35aefabfe6f291c69f
darkstarth <kiluconsta@users.noreply.github.com>
2026-08-07 02:43:20 -0500

    data: add link to gifs

--- data/gifs.js
+++ data/gifs.js
@@ -522,4 +522,5 @@ var IMGS = [
       "https://lh3.googleusercontent.com/pw/AP1GczMjm9iw_I1C0oWyGl9oPa07pp4GbfF6fTgdHb3n63bSGWSlMHJNra8p31wWsKr-j_T_MyxyKLrfuQkRKl606APG3GbcsSonRQ6OvCDu3BZnnpC4KPSL_DCn3IOVfoh0R9IvtoDXhTAk-DO555BNCTy9=w2000",
       "https://lh3.googleusercontent.com/pw/AP1GczNFBz2IarQja17b1tPS8omzswcx_PwCksS6rt7rvLQbbVNvIpjVXVp0neRTZ7W5m3MP79LzrdS_Xzq8zQQxn3eccoPzDdS7IS7WkLi5cX2SsI572XtbCMpECB8h4fpDDrP5kYKmxtSCJdOSXwUEExIY=w2000",
       "https://lh3.googleusercontent.com/pw/AP1GczM7ngXZtu1TgbnWBtYBlF5hmCHZAZWL97f025JCcUTm2wAhLzoLNDoOWPmcAy3IBeagCXRvLIGrQu0ihmA8tbsGu7u9tqB85BiwMKLQAC7os2afgLPcD2jjQ2TEluDXHIct4TXcXlT1Dvw947ZbpY7i=w2000",
+      "https://lh3.googleusercontent.com/pw/AP1GczPnLC230UxN4aNWamW-8xoQ8lTeE2C5JZEDHyygvap_xczd8ok9qRgUvmzQ1b45W15wUMj_ftNibDm1HrzvypVcqmAXFwhBSbOBXkFAPuwnH-JLdCO_kJi-hcER3WXShYGyps7Xq5W4HwkOq87ItoQ_=w2000",
 ];
